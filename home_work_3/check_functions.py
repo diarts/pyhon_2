68087a2d7a9e3dfe9b0926b0e5f42230f286486c
diarts <diarts@mail.ru>
2019-01-05 11:02:40 +0300

    finish unit tests for IpAndPortChecker class

--- home_work_3/check_functions.py
+++ home_work_3/check_functions.py
@@ -1,101 +1,84 @@
 class IpAndPortChecker:
+    _MAX_VALUE_IP = 255
+    _MIN_PORT = 1025
+    _MAX_PORT = 65535
+    _WRONG_VARIABLE_MESS = 'Вы неправильно указали переменные для запуска, если вам требуется помощь для запуска' \
+                           'программы воспользуйтесь параметром -help'
+    _WRONG_IP_MESS = 'Указан неверный IP адресс, IP адресс должен иметь вид X.X.X.X, где каждый X это число от 0 - 255'
+    _WRONG_PORT_MESS = 'Порт для подключения должен быть числом в диапазоно 1025 - 65535'
 
-    def __init__(self, summoner='программа', help_function=None, help_parameter='help'):
-        self._help_parameter = help_parameter
+    def __init__(self, help_function=None):
         self._init_help_function(help_function)
-        self._summoner = summoner
+        self._my_value_error_ip = ValueError(self.ejection_program(eject_type='ip'))
+        self._my_value_error_port = ValueError(self.ejection_program(eject_type='port'))
+        self._my_value_error_variable = ValueError(self.ejection_program(eject_type='variable'))
 
-        self._max_value_ip = 255
-        self._min_port = 1025
-        self._max_port = 65535
-        self._wrong_variable_mess = 'Вы неправильно указали переменные для запуска, ' \
-                                    f'если вам требуется помощь для запуска {self._summoner}, ' \
-                                    f'воспользуйтесь параметром {self._help_parameter}'
-        self._wrong_ip_mess = 'Указан неверный IP адресс, IP адресс должен иметь вид X.X.X.X, ' \
-                              'где каждый X это число от 0 - 255'
-        self._wrong_port_mess = 'Порт для подключения должен быть числом в диапазоно 1025 - 65535'
-
+    # has unittest
     def _init_help_function(self, my_function):
         if not my_function:
             self._help_function = self._default_help_function
-        elif my_function is function or my_function is classmethod:
+        elif hasattr(my_function, '__call__'):
             self._help_function = my_function
         else:
-            print('В параметр help function была передана не функция')
-            exit(1)
+            raise ValueError('В параметр help function была передана не функция')
 
     def _default_help_function(self):
         print('help')
 
-    def ejection_program(self, eject_type='main'):
-        if eject_type == 'id':
-            print(self._wrong_ip_mess)
+    # has unittest
+    def ejection_program(self, eject_type):
+        if eject_type == 'ip':
+            return self._WRONG_IP_MESS
         elif eject_type == 'port':
-            print(self._wrong_port_mess)
-        elif eject_type == 'main':
-            print(self._wrong_variable_mess)
+            return self._WRONG_PORT_MESS
+        elif eject_type == 'variable':
+            return self._WRONG_VARIABLE_MESS
         else:
-            print('eject_type был указан не верно, причина выхода не ясна')
-        exit(1)
+            raise ValueError('eject_type был указан не верно')
 
     # has unittest
     def check_ip_counts(self, count: int):
-        if count <= self._max_value_ip:
+        if type(count) == int and count <= self._MAX_VALUE_IP:
             return True
         else:
-            self.ejection_program(eject_type='ip')
+            raise self._my_value_error_ip
 
     # has unittest
     def ip_split(self, ip: str, splitter: str):
         try:
             return ip.split(splitter)
         except AttributeError:
-            self.ejection_program(eject_type='ip')
+            raise self._my_value_error_ip
 
     # has unittest
-    def ip_list_convert(self, ip):
+    def ip_list_items_convert_to_int(self, ip):
         for index, item in enumerate(ip):
             try:
                 new_item = int(item)
             except ValueError:
-                self.ejection_program(eject_type='id')
+                raise self._my_value_error_ip
             else:
                 self.check_ip_counts(new_item)
                 ip[index] = new_item
         return ip
 
-    def pars_ip(self, ip: str):
+    # has unittest
+    def check_ip(self, ip: str):
         ip = self.ip_split(ip=ip, splitter='.')
-        ip = self.ip_list_convert(ip=ip)
+        ip = self.ip_list_items_convert_to_int(ip)
         return ip
 
-    def check_ip(self, ip: str):
-        """check is input a ip or some string. If input not ip, close client"""
-        ip = self.pars_ip(ip)
-        for value in ip:
-            self.check_ip_counts(count=value)
-
     # has_unittest
-    def check_port_range(self, port):
-        if port is not int:
-            try:
-                port = int(port)
-            except ValueError:
-                self.ejection_program(eject_type='port')
-
-        if self._min_port <= port <= self._max_port:
-            return True
-        else:
-            self.ejection_program(eject_type='port')
-
     def check_port(self, port):
-        """check is input a socket port or some string. If input not port, close client"""
         try:
             port = int(port)
         except ValueError:
-            self.ejection_program(eject_type='port')
+            raise self._my_value_error_port
 
-        self.check_port_range(port)
+        if self._MIN_PORT <= port <= self._MAX_PORT:
+            return port
+        else:
+            raise self._my_value_error_port
 
     # has unittest
     def get_index_sys_arg(self, key, my_system_args):
@@ -106,11 +89,12 @@ class IpAndPortChecker:
         else:
             return index
 
+    # has unittest
     def get_variable(self, my_system_args, index):
         try:
             new_var = my_system_args[index + 1]
         except IndexError:
-            self.ejection_program()
+            raise self._my_value_error_variable
         else:
             return new_var
 
@@ -121,15 +105,16 @@ class IpAndPortChecker:
             self.check_port(new_var)
         return new_var
 
+    # has unittest
     def add_variables(self, my_system_args, my_variables):
         for key in my_variables.keys():
             if key in my_system_args:
-                index = self.get_index_sys_arg(key=key, my_system_args=my_system_args)
+                index = self.get_index_sys_arg(key, my_system_args)
                 if not index:
                     continue
 
-                new_var = self.get_variable(my_system_args=my_system_args, index=index)
-                my_variables[key] = self.check_new_var(key=key, new_var=new_var)
+                new_var = self.get_variable(my_system_args, index)
+                my_variables[key] = self.check_new_var(key, new_var)
         return my_variables
 
     def check_sys_args(self, my_system_args, my_variables):
@@ -140,7 +125,7 @@ class IpAndPortChecker:
 
         if len(my_system_args) == 2 and my_system_args[1] == 'help':
             self._help_function()
-            exit(0)
+            return False
 
         elif len(my_system_args) == 1:
             return my_variables
@@ -152,7 +137,12 @@ class IpAndPortChecker:
 
 if __name__ == '__main__':
     import sys
+
     sys_args = sys.argv
 
-    a = IpAndPortChecker('aaaaa')
-    print(a.check_sys_args(sys_args, {'-a': 'localhost', '-p': 7777, '-un': 'Vasiliy Pupckina'}))
+    checker = IpAndPortChecker()
+    my_variables = checker.check_sys_args(sys_args, {'-a': 'localhost', '-p': 7777, '-un': 'Vasiliy Pupckina'})
+    if not my_variables:
+        exit(0)
+    else:
+        print(my_variables)
--- home_work_3/unit_tests/test_check_functions.py
+++ home_work_3/unit_tests/test_check_functions.py
@@ -1,81 +1,141 @@
 import unittest
 import check_functions
 
-class IpAndPortCheckerMok(check_functions.IpAndPortChecker):
-    def ejection_program(self, eject_type='main'):
-        raise Exception
 
-
-class TestIpSplit(unittest.TestCase):
+class TestIpAndPortChecker(unittest.TestCase):
     def setUp(self):
-        self.a = check_functions.IpAndPortChecker()
+        self.ip_port_checker = check_functions.IpAndPortChecker()
         self.spliter = '.'
+        self.my_sys_args = ['path', 'some', 'things', '-a', '255.255.255.255']
+        self.my_var = {'-a': 'localhost', '-p': 7777, '-un': 'Vasiliy Pupckin'}
 
-    def test_split(self):
+    def test_init_help_function_failed(self):
+        test_function = 1234
+        self.assertRaises(ValueError, self.ip_port_checker._init_help_function, test_function)
+
+    def test_ejection_programm_main_success(self):
+        eject_type = 'variable'
+        returned_string = self.ip_port_checker.ejection_program(eject_type)
+        self.assertIs(type(returned_string), str)
+        self.assertEqual(returned_string, self.ip_port_checker._WRONG_VARIABLE_MESS)
+
+    def test_ejection_program_ip_success(self):
+        eject_type = 'ip'
+        returned_string = self.ip_port_checker.ejection_program(eject_type)
+        self.assertIs(type(returned_string), str)
+        self.assertEqual(returned_string, self.ip_port_checker._WRONG_IP_MESS)
+
+    def test_ejection_program_port_success(self):
+        eject_type = 'port'
+        returned_string = self.ip_port_checker.ejection_program(eject_type)
+        self.assertIs(type(returned_string), str)
+        self.assertEqual(returned_string, self.ip_port_checker._WRONG_PORT_MESS)
+
+    def test_ejection_program_wrong_argument(self):
+        eject_type = 124131
+        self.assertRaises(ValueError, self.ip_port_checker.ejection_program, eject_type)
+        eject_type = ''
+        self.assertRaises(ValueError, self.ip_port_checker.ejection_program, eject_type)
+        eject_type = [4, 5, 584]
+        self.assertRaises(ValueError, self.ip_port_checker.ejection_program, eject_type)
+
+    def test_check_ip_success(self):
+        ip = [255, 255, 255, 255]
+        for item in ip:
+            self.assertEqual(self.ip_port_checker.check_ip_counts(item), True)
+
+    def test_check_ip_failed_float(self):
+        ip_segment = 235.54
+        self.assertRaises(ValueError, self.ip_port_checker.check_ip_counts, ip_segment)
+        ip_segment = 'some things'
+        self.assertRaises(ValueError, self.ip_port_checker.check_ip_counts, ip_segment)
+        ip_segment = 256
+        self.assertRaises(ValueError, self.ip_port_checker.check_ip_counts, ip_segment)
+
+    def test_ip_split_success(self):
         ip = '255.255.255.255'
-        self.assertEqual(self.a.ip_split(ip=ip, splitter=self.spliter), ['255', '255', '255', '255'])
+        model = ['255', '255', '255', '255']
+        self.assertEqual(self.ip_port_checker.ip_split(ip=ip, splitter=self.spliter), model)
 
-    def test_wrong_split_1(self):
+    def test_ip_split_wrong(self):
         ip = '255.255.255.2555'
-        self.assertNotEqual(self.a.ip_split(ip=ip, splitter=self.spliter), ['255', '255', '255', '255'])
+        model = ['255', '255', '255', '255']
+        self.assertNotEqual(self.ip_port_checker.ip_split(ip=ip, splitter=self.spliter), model)
+        model = [255, 255, 255, 255]
+        self.assertNotEqual(self.ip_port_checker.ip_split(ip, self.spliter), model)
 
-    def test_wrong_split_2(self):
-        ip = '255.255.255.255'
-        self.assertNotEqual(self.a.ip_split(ip, self.spliter), [255, 255, 255, 255])
-
-
-class TestIpListConvert(unittest.TestCase):
-    def setUp(self):
-        self.a = IpAndPortCheckerMok()
+    def test_ip_slit_failed(self):
+        ip = 2313143242423424
+        self.assertRaises(ValueError, self.ip_port_checker.ip_split, ip, self.spliter)
 
     def test_convert_successful(self):
         ip = ['255', '255', '255', '255']
-        self.assertEqual(self.a.ip_list_convert(ip), [255, 255, 255, 255])
+        model = [255, 255, 255, 255]
+        self.assertEqual(self.ip_port_checker.ip_list_items_convert_to_int(ip), model)
 
     def test_convert_return_is_list(self):
         ip = ['255', '255', '255', '255']
-        self.assertIs(type(self.a.ip_list_convert(ip)), list)
+        self.assertIs(type(self.ip_port_checker.ip_list_items_convert_to_int(ip)), list)
 
     def test_convert_return_not_dict(self):
         ip = ['255', '255', '255', '255']
-        self.assertIsNot(type(self.a.ip_list_convert(ip)), dict)
+        self.assertIsNot(type(self.ip_port_checker.ip_list_items_convert_to_int(ip)), dict)
 
-    def test_convert_raise_exept(self):
-        ip = ['dsfasds', '255', '255', '255']
-        self.assertRaises(Exception, self.a.ip_list_convert, ip)
+    def test_convert_failed(self):
+        ip = [45453.45, '255', '255', '255']
+        self.assertRaises(ValueError, self.ip_port_checker.ip_list_items_convert_to_int, ip)
 
-class TestCheckIpCount(unittest.TestCase):
-    def setUp(self):
-        self.a = IpAndPortCheckerMok()
+    def test_pars_ip_success(self):
+        ip = '255.255.255.255'
+        model = [255, 255, 255, 255]
+        self.assertEqual(self.ip_port_checker.check_ip(ip), model)
 
-    def test_check_successful(self):
-        ip = [255, 255, 255, 255]
-        for item in ip:
-            self.assertEqual(self.a.check_ip_counts(item), True)
+    def test_pars_ip_failed(self):
+        ip = 'some thing'
+        self.assertRaises(ValueError, self.ip_port_checker.check_ip, ip)
+        ip = 53457132154
+        self.assertRaises(ValueError, self.ip_port_checker.check_ip, ip)
 
-class TestPortRange(unittest.TestCase):
-    def setUp(self):
-        self.a = IpAndPortCheckerMok()
-
-    def test_port_range_right(self):
+    def test_port_range_success(self):
         port = 3582
-        self.assertEqual(self.a.check_port_range(port), True)
+        self.assertEqual(self.ip_port_checker.check_port(port), port)
 
-    def test_port_range_wront_input(self):
-        port = 'sdfsfsdf'
-        self.assertRaises(Exception, self.a.check_port_range, port)
-
-class TestGetIndexSysArgs(unittest.TestCase):
-    def setUp(self):
-        self.a = IpAndPortCheckerMok()
+    def test_port_range_failed(self):
+        port = 'some thing'
+        self.assertRaises(ValueError, self.ip_port_checker.check_port, port)
+        port = 1231214124
+        self.assertRaises(ValueError, self.ip_port_checker.check_port, port)
 
     def test_get_index_successfull(self):
-        my_args = ['path', '-a', '255.255.255.255']
-        self.assertEqual(self.a.get_index_sys_arg('-a', my_args), 1)
+        key = '-a'
+        self.assertEqual(self.ip_port_checker.get_index_sys_arg(key, self.my_sys_args), 3)
 
     def test_get_index_wrong_argument(self):
-        my_args = ['path', '-a', '255.255.255.255']
-        self.assertNotEqual(self.a.get_index_sys_arg('-c', my_args), 1)
+        key = '-c'
+        self.assertEqual(self.ip_port_checker.get_index_sys_arg(key, self.my_sys_args), False)
+        key = 46421
+        self.assertEqual(self.ip_port_checker.get_index_sys_arg(key, self.my_sys_args), False)
+
+    def test_get_variable_success(self):
+        index = 3
+        model_var = '255.255.255.255'
+        self.assertEqual(self.ip_port_checker.get_variable(self.my_sys_args, index), model_var)
+
+    def test_get_variable_failed(self):
+        index = 4
+        self.assertRaises(ValueError, self.ip_port_checker.get_variable, self.my_sys_args, index)
+
+    def test_add_variables_success(self):
+        my_sys_args = ['path', 'some', 'things', '-a', '255.255.255.255']
+        my_var = {'-a': 'localhost', '-p': 7777, '-un': 'Vasiliy Pupckin'}
+        model_dict = {'-a': '255.255.255.255', '-p': 7777, '-un': 'Vasiliy Pupckin'}
+        self.assertEqual(self.ip_port_checker.add_variables(my_sys_args, self.my_var), model_dict)
+
+    def test_add_variables_failed(self):
+        my_sys_args = ['path', 'some', 'things', '-a', '368.255.255.255']
+        self.assertRaises(ValueError, self.ip_port_checker.add_variables, my_sys_args, self.my_var)
+
+
 
 if __name__ == '__main__':
     unittest.main()
